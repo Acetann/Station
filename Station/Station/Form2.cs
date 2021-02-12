9a65fffd0550388068a3aba2e3b2ec75d8c3a80b
Acetann <58683954+Acetann@users.noreply.github.com>
2021-02-12 09:14:42 +0100

    Add files via upload

---
+++ Station/Station/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Station
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 x = new Form3();
+            x.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 x = new Form3();
+            x.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 x = new Form3();
+            x.Show();
+        }
+    }
+}
